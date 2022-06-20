7104547093f8cb1186911c0d7c19c30f6880e487
zenjause <jesus.nf22@gmail.com>
2022-06-20 15:32:38 -0500

    recursividad

--- intermedio.js
+++ intermedio.js
@@ -79,7 +79,7 @@ const obj4 = Object.create(obj1);*/
 //JSON.parse y JSON.stringify
 
 
-const obj1 = {
+/*const obj1 = {
     a:"a",
     b:"b",
     c:{
@@ -92,4 +92,31 @@ const obj1 = {
 
 
 const stringifiedComplexObj = JSON.stringify(obj1);
-const obj2 = JSON.parse(stringifiedComplexObj);
+const obj2 = JSON.parse(stringifiedComplexObj);*/
+
+
+//recursividad 
+
+/*function recursiva (){
+    if(){
+
+    }else{
+        
+    }
+};*/
+
+const numeritos = [0,1,2,3,4,5,6,7,8,9,234,211,547,312,53432,23,32,8,643,3];
+/*let numerito = 0;
+for(let index = 0; index < numeritos.length; index++){
+    numerito = numeritos [index];
+    console.log({index,numerito});
+}*/
+
+function recursiva(numbersArray){
+    if (numbersArray.length != 0){
+        const firstNum = numbersArray[0];
+        console.log(firstNum);
+        numbersArray.shift();
+        recursiva(numbersArray);
+    }
+};
