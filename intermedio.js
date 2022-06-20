2316fb64fc674b5136225770be782af253cbd414
zenjause <jesus.nf22@gmail.com>
2022-06-20 16:01:36 -0500

    recursividad con deep copy

--- intermedio.js
+++ intermedio.js
@@ -86,6 +86,9 @@ const obj4 = Object.create(obj1);*/
         d:"d",
         e:"e",
     },
+    editA(){
+        this.a = "AAAAAA";
+    }
 
     
 };
@@ -105,18 +108,76 @@ const obj2 = JSON.parse(stringifiedComplexObj);*/
     }
 };*/
 
-const numeritos = [0,1,2,3,4,5,6,7,8,9,234,211,547,312,53432,23,32,8,643,3];
+//const numeritos = [0,1,2,3,4,5,6,7,8,9,234,211,547,312,53432,23,32,8,643,3];
 /*let numerito = 0;
 for(let index = 0; index < numeritos.length; index++){
     numerito = numeritos [index];
     console.log({index,numerito});
 }*/
 
-function recursiva(numbersArray){
+/*function recursiva(numbersArray){
     if (numbersArray.length != 0){
         const firstNum = numbersArray[0];
         console.log(firstNum);
         numbersArray.shift();
         recursiva(numbersArray);
     }
+};*/
+
+
+// recursividad  deep copy 
+const obj1 = {
+    a:"a",
+    b:"b",
+    c:{
+        d:"d",
+        e:"e",
+    },
+    editA(){
+        this.a = "AAAAAA";
+    }
+
+    
 };
+
+function isObject(subject){
+    return typeof subject == "object";
+}
+function isArray(subject){
+    return Array.isArray(subject);
+}
+
+function deepCopy(subject){
+    let copySubject;
+
+    const subjectIsObject = isObject(subject);
+    const subjectIsArray = isArray(subject);
+
+
+    if(subjectIsArray){
+        copySubject = [];
+    }else if(subjectIsObject){
+        copySubject = {};
+    }else{
+        return subject;
+    }
+    
+    for (key in subject){
+        const keyIsObject = isObject(subject[key]);
+
+        if(keyIsObject){
+            copySubject[key] = deepCopy(subject[key]);
+        }else{
+            if(subjectIsArray){
+                copySubject.push(subject[key]);
+            }else{
+                copySubject[key] = subject[key];
+            }
+        }
+    }
+
+
+    return copySubject;
+};
+
+
